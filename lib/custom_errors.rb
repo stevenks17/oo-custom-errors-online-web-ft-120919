90a39beb09c58c2fbd4651494390f7f2b1c4b812
Steven Kumarsingh <stevenks17@gmail.com>
2020-01-11 20:54:38 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -5,17 +5,18 @@ class Person
     @name = name
   end
 
-  def get_married(person)
+ def get_married(person)
     self.partner = person
     if person.class != Person
       begin
-         raise YourCustomError
-         rescue YourCustomError
-       end
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
     else
       person.partner = self
     end
-  end
+ end
 
  class PartnerError < StandardError
     def message
